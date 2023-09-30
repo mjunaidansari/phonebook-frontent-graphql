2a8498cb31da72abb12b6b004e5e349acecaa7d9
Junaid Ansari <junaidshahid.ansari@gmail.com>
2023-09-30 19:48:45 +0530

    subscription on client

--- src/index.js
+++ src/index.js
@@ -10,39 +10,39 @@ import { getMainDefinition } from "@apollo/client/utilities";
 import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
 import { createClient } from 'graphql-ws'
 
-const authLink = setContext( (_, { headers }) => {
+const authLink = setContext( (_, {headers}) => {
+
 	const token = localStorage.getItem('phonenumbers-user-token')
 	return {
-		headers: {
+		headers : {
 			...headers,
-			authorization: token ? `Bearer ${token}` : null,
+			authorizatoin: token ? `Bearer ${token}` : null,
 		}
 	}
-})
 
-const httpLink = createHttpLink({
-	uri: 'http://localhost:4000',
-})
+} )
+
+const httpLink  = createHttpLink({uri: 'http://localhost:4000'})
 
 const wsLink = new GraphQLWsLink(
 	createClient({url: 'ws://localhost:4000'})
 )
 
-const splitlink = split(
+const splitLink = split(
 	({query}) => {
 		const definition = getMainDefinition(query)
 		return (
 			definition.kind === 'OperationDefinition' &&
 			definition.operation === 'subscription'
-		),
-		wsLink,
-		authLink.concat(httpLink)
-	}
+		)
+	},
+	wsLink,
+	authLink.concat(httpLink)
 )
 
 const client = new ApolloClient({
 	cache: new InMemoryCache(),
-	link: splitlink
+	link: splitLink	
 })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
